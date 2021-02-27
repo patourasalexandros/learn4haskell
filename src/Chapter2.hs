bbb14fe93ba6ba8d8bfa0dce501d6b3cd743ff8f
Your Name <you@example.com>
2021-02-28 01:07:31 +0200

    contains implemented

--- src/Chapter2.hs
+++ src/Chapter2.hs
@@ -773,7 +773,9 @@ the list with only those lists that contain a passed element.
 
 🕯 HINT: Use the 'elem' function to check whether an element belongs to a list
 -}
-contains = error "contains: Not implemented!"
+contains:: Int -> [[Int]] -> [[Int]]
+contains x list = filter (elem x) list
+
 
 
 {- |

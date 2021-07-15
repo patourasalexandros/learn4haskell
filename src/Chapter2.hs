13f29a8bfcc1084ba25e4c6fd9412c476d092068
patal <patourasalexandros@gmail.com>
2021-07-16 00:32:02 +0300

    rewind

--- src/Chapter2.hs
+++ src/Chapter2.hs
@@ -876,7 +876,13 @@ list.
 
 🕯 HINT: Use the 'cycle' function
 -}
-rotate = error "rotate: Not implemented!"
+rotate:: Int -> [a] -> [a]
+rotate n l
+  | n < 0 = []
+  | null l = []
+  | otherwise = 
+    let len = length l
+    in take len (drop (mod n len) (cycle l)) 
 
 {- |
 =💣= Task 12*
@@ -892,7 +898,9 @@ and reverses it.
   function, but in this task, you need to implement it manually. No
   cheating!
 -}
-rewind = error "rewind: Not Implemented!"
+rewind:: [a] -> [a]
+rewind [] = []
+rewind (x:xs) = rewind xs ++ [x]
 
 
 {-
